dede9d1c5de26c9a2fca9b4f2f4322b030f360b1
jopfann <johannes.pfann@novum-online.de>
2013-03-07 09:49:10 +0100

    - Added new Class TestKlasse

---
+++ gittestprojekt/src/main/java/de/novum/qa/TestKlasse.java
@@ -0,0 +1,12 @@
+package de.novum.qa;
+
+public class TestKlasse {
+
+    /**
+     * @param args
+     */
+    public static void main(final String[] args) {
+        System.out.println("Mal schaun!");
+    }
+
+}
